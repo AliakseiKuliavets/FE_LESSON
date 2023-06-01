9fae7e525c071e731a9046fd139bb19ac934947e
AliakseiKuliavets <132812649+AliakseiKuliavets@users.noreply.github.com>
2023-06-01 12:56:50 +0200

    Add files via upload

---
+++ Project7/script.js
@@ -0,0 +1,39 @@
+alert("hello") // alert это уведомление 
+console.log("hello") // отображение консоли 
+const name = prompt("What your name?") // функция для ввода информации 
+console.log(name)
+
+const userAge = 40 
+const userLogin = "Starta Institute"
+console.log(userAge)
+console.log("userAge")
+
+const nameProducts = prompt("The product's name")
+console.log(nameProducts)
+
+const userName = prompt("Username")
+console.log(userName)
+
+const productPrice = prompt("Product price")
+console.log(productPrice)
+
+
+
+
+
+
+
+
+
+/* Правила наименования переменных:
+            1. латиница - name, age, text, title
+            2. в переменной могут использоваться цифры, но только не в начале - 
+                name1 - правильно
+                1name - неправильно
+            3. переменная должна быть осмысленной
+                Если переменная хранит имя - name
+                Если возраст - age
+            4. camelCase - первая буква первого слова - маленькая, первые буквы остальных словв - заглавные
+                переменная хранит цену продукта - productPrice
+                переменная хранит цену продукта со скидкой - productPriceWithSale
+*/

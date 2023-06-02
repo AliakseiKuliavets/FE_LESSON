c075a1f338f7f916414987dac3f0d907f124d826
AliakseiKuliavets <132812649+AliakseiKuliavets@users.noreply.github.com>
2023-06-02 12:48:37 +0200

    Add files via upload

---
+++ Project7(1)/script.js
@@ -0,0 +1,58 @@
+// let number = 10;
+// let result = number ** 4;
+
+// console.log(result);
+// alert(result);
+
+// let cel = prompt ('введите *C');
+// let result = cel * 1.8 + 32;
+// console.log(result);
+// alert(result);
+
+// let time = prompt ('сколько часов');
+// let result = time % 24 ;
+// console.log(result);
+// alert(result);
+
+// let order_cost = 1000;
+// let discount = prompt ("discount");
+// let margin = prompt ("margin");
+// let result_discount = order_cost - order_cost * ( discount / 100 );
+// let resultmargin = order_cost + order_cost * ( margin / 100 );
+// console.log(result_discount);
+// console.log(resultmargin);
+
+// let a = +prompt ("number a");
+// let b = +prompt ("number b");
+// let c = +prompt ("number c");
+// let result = (a + b + c);
+// console.log(result / 3);
+
+// let a = +prompt ("number a");
+// let result = a - a * (1 / 100);
+// console.log(result);
+
+// let order_cost = 1000;
+// let order_paid = 750;
+// let result = order_paid / order_cost * 100;
+// console.log(100 - result);
+
+
+
+
+
+
+
+
+/* Правила наименования переменных:
+            1. латиница - name, age, text, title
+            2. в переменной могут использоваться цифры, но только не в начале - 
+                name1 - правильно
+                1name - неправильно
+            3. переменная должна быть осмысленной
+                Если переменная хранит имя - name
+                Если возраст - age
+            4. camelCase - первая буква первого слова - маленькая, первые буквы остальных словв - заглавные
+                переменная хранит цену продукта - productPrice
+                переменная хранит цену продукта со скидкой - productPriceWithSale
+*/

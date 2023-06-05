5b38a46703cc6568acb6ec18fec46174140c6bde
AliakseiKuliavets <132812649+AliakseiKuliavets@users.noreply.github.com>
2023-06-05 11:22:28 +0200

    Add files via upload

--- Project7/script.js
+++ Project7/script.js
@@ -1,21 +1,78 @@
-alert("hello") // alert это уведомление 
-console.log("hello") // отображение консоли 
-const name = prompt("What your name?") // функция для ввода информации 
-console.log(name)
+// alert("hello") // alert это уведомление 
+// console.log("hello") // отображение консоли 
+// const name = prompt("What your name?") // функция для ввода информации 
+// console.log(name)
 
-const userAge = 40 
-const userLogin = "Starta Institute"
-console.log(userAge)
-console.log("userAge")
+// const nameProducts = prompt("The product's name")
+// console.log(nameProducts)
 
-const nameProducts = prompt("The product's name")
-console.log(nameProducts)
+// const userName = prompt("Username")
+// console.log(userName)
 
-const userName = prompt("Username")
-console.log(userName)
+// const productPrice = prompt("Product price")
+// console.log(productPrice)
 
-const productPrice = prompt("Product price")
-console.log(productPrice)
+// const userAge = 40 
+// const userLogin = "Starta Institute"
+// console.log(userAge)
+// console.log("userAge")
+
+// let price = 2000;
+// const price1 = 3000;
+
+// price = 5000;
+// price1 = 7000;
+// console.log(price);
+// console.log(price1);
+
+
+// // Задача. Запросить два числа у пользователя и в консоли отобразить их сумму.
+// const number = +prompt("Введите первое число"); // запросили первое число и сохранили в переменную number
+// const number1 = +prompt("Введите второе число"); // запросили второе число и сохраниил в number1
+// console.log(number + number1); // отображаем пользователю сумму двух чисел
+
+
+// Задача. Запросить у пользователся число. Вконсоли вывести квадрат этого числа.
+// const number = +prompt("What number");
+// console.log(number * number);
+// console.log(number ** 2) // number ** 3, number ** 4 
+// console.log(Math.pow(number, 2)) // Math.pow(число, степень)
+
+
+// Задача. Запросить у пользователя имя. Вконсоли вывести "Hello, {Name}"
+// const name = prompt("What  your name");
+// hello = "Hello, ";
+// console.log(hello + name);
+// console.log("Hello, " + name); // конкатенация
+// console.log(`Hello, ${name}`); // интерполяция
+
+
+// // Задача. Запросить у пользователя имя и возраст. В консоли вывести
+// // "Hello, {name}. You are {age} yers old"
+// const name = prompt("What  your name");
+// const age = prompt("How old are you");
+
+// console.log("Hello, " + name + ". You are " + age + " years old");
+// console.log(`Hello, ${name}. You are ${age} years old`);
+
+
+// Задача. Есть переменные перевести r,g,b. В консоль вывести строку "rgb (r, g, b)"
+// rgb(15, 51, 120)
+// const r = 15;
+// const g = 51;
+// const b = 120;
+
+// console.log(`rgb(${r}, ${g}, ${b})`);
+
+
+// /*
+//     конкатенация - склеивание строк. 
+//     prompt всегда возвращает строку, чтобы получить число - нужно поставить + перед prompt()
+// */
+
+
+
+
 
 
 

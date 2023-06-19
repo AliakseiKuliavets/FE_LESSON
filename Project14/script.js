33da36c92be320cad92abd9f0ce1cf8b9ed1eb81
AliakseiKuliavets <132812649+AliakseiKuliavets@users.noreply.github.com>
2023-06-19 12:53:19 +0200

    Add files via upload

--- Project14/script.js
+++ Project14/script.js
@@ -76,27 +76,29 @@
 
 
 
-const red = document.querySelector(".red-btn")
-const green = document.querySelector(".green-btn")
-const blue = document.querySelector(".blue-btn")
-const yellow = document.querySelector(".yellow-btn")
-red.addEventListener("click", function() {
-    document.body.style.backgroundColor = "green" 
-})
-green.addEventListener("click", function() {
-    document.body.style.backgroundColor = "blue" 
-})
-blue.addEventListener("click", function() {
-    document.body.style.backgroundColor = "yellow" 
-})
-yellow.addEventListener("click", function() {
-    document.body.style.backgroundColor = "red" 
-})
+// const red = document.querySelector(".red-btn")
+// const green = document.querySelector(".green-btn")
+// const blue = document.querySelector(".blue-btn")
+// const yellow = document.querySelector(".yellow-btn")
+// red.addEventListener("click", function() {
+//     document.body.style.backgroundColor = "red" 
+// })
+// green.addEventListener("click", function() {
+//     document.body.style.backgroundColor = "green" 
+// })
+// blue.addEventListener("click", function() {
+//     document.body.style.backgroundColor = "blue" 
+// })
+// yellow.addEventListener("click", function() {
+//     document.body.style.backgroundColor = "yellow" 
+// })
 
-const img = document.querySelector(".img-btn")
-img.addEventListener("click", function() {
-    document.body.style.background = "url(123.png)";
-})
+// const img = document.querySelector(".img-btn")
+// img.addEventListener("click", function() {
+//     document.body.style.background = "url(123.png)";
+// })
+
+
 
 // ДЗ. При нажатии на каждую из кнопок менять цвет фона body на соответвующий
 //         При нажатии на кнопку img-btn менять фон body на картинку.
@@ -107,16 +109,81 @@ img.addEventListener("click", function() {
 
 
 
+// const buttons = document.querySelectorAll("button")
 
-// main = 
-//     {
-//         innerHTML: "<button></button>",
-//         style: {
-//             backgroundColor: ""
+// for (let i = 0; i < buttons.length; i++) {
+//     buttons[i].addEventListener("click", function () {
+//         if (buttons[i].id == "img") {
+//             document.body.style.background = "url(123.png)"
+//         } else {
+//             const color = buttons[i].id
+//             document.body.style.background = color
 //         }
+//     })
+// }
+
+
+// // переписать код используя FOREACH вместо FOR
+// buttons.forEach(function (button) {
+//     button.addEventListener("click", function () {
+//         if (button.id == "img") {
+//             document.body.style.background = "url(123.png)"
+//         } else {
+//             const color = button.id
+//             document.body.style.background = color
+//         }
+//     })
+// })
+
+
+
+// // Есть массив names, нужно отобразить каждый элемент массива в консоли
+// const names = ["Arsen", "Bogdan", "Aliaksei", "Botirjon"]
+
+// // 1.
+// for(let i=0; i < names.length; i++) {
+//     console.log(names[i])
+// }
+
+// //2.
+// names.forEach(function(name) {
+//     console.log(name)
+// })
+
+// /*
+//     .forEach() - еще один способ выполнить операцию для каждого элемента массива, но
+//         без явного описания цикла, как было в for
+//     позволяет применить коллбек-функцию ко всем элементам массива.
+
+//     forEach(function(item, index, arr){}) принимает 1 аргумент  - колбек принимает три аргумента
+//         1. item - каждый элемент массива, текущий элемент; (array[i]) - каждый элемент массива в for
+//         2. index -  индекс текущего элемента
+//         3. arr - сам массив, над которым выполняем действия
+// */
+
+
+
+
+// // Есть массив чисел numbers. FOREACH
+// const numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+// // вывести в консоли
+// // а) вывести числа больше 60
+// // б) квадрат каждого числа
+
+// numbers.forEach(function (number) {
+//     if (number > 60) {
+//         console.log(number)
 //     }
+// })
+// numbers.forEach(function (number) {
+//         console.log(number ** 2)
+// })
 
 
-// Задача. При нажатии на кнопки менять цвет фона body на соответсвующий
-// const red = document.querySelector(".red-btn")
+
+
+
+
+
+
 
